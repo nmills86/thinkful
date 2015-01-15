948d64a69865adfd6c1712b4fe04ba5a33b02502
Nick Mills <nickrgmills@gmail.com>
2015-01-14 20:27:58 -0800

    Add Database.PY

---
+++ database.py
@@ -0,0 +1,14 @@
+# Assignment 3 for Unit 1
+
+#Import Packages
+
+#Connect to Database
+
+#Create Tables
+
+#Join Data
+
+#Load Data into panda DATAFRAME
+
+#Print Cities that have Warmest Month in July
+
